a51cd1e1f4dd0fee352cbace29126bbfc8500ef5
Ramkumar96 <sivaram0203@gmail.com>
2019-09-13 14:40:01 +0530

    new react

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CreateTodo extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to create Todo Component !!</p>
+            </div>
+        )
+    }
+}
+
+export default CreateTodo;
